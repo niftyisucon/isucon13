c1ce795d2afdb32606852e646fc3d35d7eeceef6
Penpen7 <Raosys@live.jp>
2023-11-25 15:44:25 +0900

    recordsに対するインデックスを追加

--- webapp/sql/initdb.d/20_index.sql
+++ webapp/sql/initdb.d/20_index.sql
@@ -1,2 +1,3 @@
 ALTER TABLE livestream_tags ADD INDEX idx_livestream_tags_livestream_id (livestream_id);
 ALTER TABLE themes ADD INDEX idx_themes_user_id (user_id);
+ALTER TABLE records ADD INDEX idx_records_disabled_name_domain_id (disabled, name, domain_id);
